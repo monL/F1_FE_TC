476b5d2dddcf5d47d1315da97cc1b41a3749f715
MonL <monzhee@gmail.com>
2018-12-13 11:35:03 -0500

    Fixed Navigation

--- app/app.js
+++ app/app.js
@@ -1,20 +1,3 @@
-
-//Fetch collection of case posts
-//CaseCollectionModel calls the mock api, parses and stores the response data
-var caseCollection = new app.CaseCollectionModel();
-caseCollection.fetch({
-    success: function() {
-        console.log('Fetched the Case collection!');
-    },
-    error: function() {
-        console.log('Something failed...');
-    }
-}).done(function(){
-    var caseCollectionView = new app.CaseCollectionView({ collection: caseCollection});
-    //Render collection information using the casecollectionView
-    $("#mainContent").html(caseCollectionView.render().el);
-});
-
 var appRouter = new app.Router();
 
 Backbone.history.start();
--- app/routes/router.js
+++ app/routes/router.js
@@ -3,11 +3,26 @@ var app = app || {};
 app.Router = Backbone.Router.extend({
 
     routes :{
-        "": "noCopy",
+        "": "home",
         "post/:id": "postDetails"
     },
 
-    noCopy: function() {
+    home: function() {
+        //Fetch collection of case posts
+        //CaseCollectionModel calls the mock api, parses and stores the response data
+        var caseCollection = new app.CaseCollectionModel();
+        caseCollection.fetch({
+            success: function() {
+                console.log('Fetched the Case collection!');
+            },
+            error: function() {
+                console.log('Something failed...');
+            }
+        }).done(function(){
+            var caseCollectionView = new app.CaseCollectionView({ collection: caseCollection});
+            //Render collection information using the casecollectionView
+            $("#mainContent").html(caseCollectionView.render().el);
+});
     },
 
     postDetails: function(id) {
